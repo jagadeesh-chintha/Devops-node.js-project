d42ae57bee2d9784929902bfc559a9676327ee3c
Jagadeesh Chintha <chinthajagadeesh123@gmail.com>
2025-07-25 20:02:12 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
+  key_name = "cicd"
+  tags = {
+    Name = "Devops-NodeAPP"
+  }
+
+  provisioner = "local-exce" {
+    command = "echo ${self.public_ip} > ../ansible/hosts"
+  }
+}
